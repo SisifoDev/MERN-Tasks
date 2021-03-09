72b7b513c1b564502abef308854a9ba9cb2b4fa9
Sisifo-G <manuelperdomo1710@icloud.com>
2021-03-09 10:26:05 -0500

    Sidebar

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -2,104 +2,104 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NuevaCuenta = () => {
-    // state para iniciar sesión
-    const [usuario, guardarUsuario] = useState({
-        nombre: "",
-        email: "",
-        password: "",
-        confirmar: "",
+  // state para iniciar sesión
+  const [usuario, guardarUsuario] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    confirmar: "",
+  });
+
+  // Extrater de Usuario
+  const { nombre, email, password, confirmar } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    // Extrater de Usuario
-    const { nombre, email, password, confirmar } = usuario;
+  // Cuando el usuario quiere iniciar sesión
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-    const onChange = (e) => {
-        guardarUsuario({
-            ...usuario,
-            [e.target.name]: e.target.value,
-        });
-    };
+    // validar que no haya campos vacíos
 
-    // Cuando el usuario quiere iniciar sesión
-    const onSubmit = (e) => {
-        e.preventDefault();
+    // Password mínimo de 6 caracteres
 
-        // validar que no haya campos vacíos
+    // Los dos password son iguales
 
-        // Password mínimo de 6 caracteres
+    // Pasarlo al action
+  };
 
-        // Los dos password son iguales
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Obtener una cuenta</h1>
 
-        // Pasarlo al action
-    };
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              placeholder="Tu Nombre"
+              value={nombre}
+              onChange={onChange}
+            />
+          </div>
 
-    return (
-        <div className="form-usuario">
-            <div className="contenedor-form sombra-dark">
-                <h1>Obtener una cuenta</h1>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
 
-                <form onSubmit={onSubmit}>
-                    <div className="campo-form">
-                        <label htmlFor="nombre">Nombre</label>
-                        <input
-                            type="text"
-                            id="nombre"
-                            name="nombre"
-                            placeholder="Tu Nombre"
-                            value={nombre}
-                            onChange={onChange}
-                        />
-                    </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
 
-                    <div className="campo-form">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Tu Email"
-                            value={email}
-                            onChange={onChange}
-                        />
-                    </div>
-
-                    <div className="campo-form">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Tu Password"
-                            value={password}
-                            onChange={onChange}
-                        />
-                    </div>
-
-                    <div className="campo-form">
-                        <label htmlFor="confirmar">Confirmar Password</label>
-                        <input
-                            type="password"
-                            id="confirmar"
-                            name="confirmar"
-                            placeholder="Repite tu Password"
-                            value={confirmar}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div className="campo-form">
-                        <input
-                            type="submit"
-                            className="btn btn-primario btn-block"
-                            value="Registrarme"
-                        />
-                    </div>
-                </form>
-                <Link to={"/"} className="enlace-cuenta">
-                    Regresar a Iniciar Sesión
-                </Link>
-            </div>
-        </div>
-    );
+          <div className="campo-form">
+            <label htmlFor="confirmar">Confirmar Password</label>
+            <input
+              type="password"
+              id="confirmar"
+              name="confirmar"
+              placeholder="Repite tu Password"
+              value={confirmar}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Registrarme"
+            />
+          </div>
+        </form>
+        <Link to={"/"} className="enlace-cuenta">
+          Regresar a Iniciar Sesión
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NuevaCuenta;
