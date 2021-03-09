480360052aa6e9418e43b066ff97b538374bd292
Sisifo-G <manuelperdomo1710@icloud.com>
2021-03-09 14:29:48 -0500

    state del  NuevoProyecto

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,17 +1,46 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NuevoProyecto = () => {
+  //state para proyecto
+  const [proyecto, guardarProyecto] = useState({
+    nombre: "",
+  });
+
+  // Extraer nombre de proyecto
+  const { nombre } = proyecto;
+
+  // Lee los contenidos del input
+  const onChangeProyecto = (e) => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Cuando el usuario hace clic en guardar proyecto
+  const onSubmitProyecto = (e) => {
+    e.preventDefault();
+
+    // VAlidar el proyecto
+
+    // Agregar al state
+
+    // Reiniciar el form
+  };
+
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
-      <form action="" className="formulario-nuevo-proyecto">
+      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
         <input
           type="text"
           className="input-text"
           placeholder="Nombre Proyecto"
           name="nombre"
+          value={nombre}
+          onChange={onChangeProyecto}
         />
         <input
           type="submit"
